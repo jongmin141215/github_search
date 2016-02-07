700ebbc1c76f25f5cf4f69cf43aa0fef05cba618
Jongmin Kim <kjm141215@gmail.com>
2016-02-06 18:01:28 -0800

    Refactor

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -1,11 +1,19 @@
 describe('Github Profile finder', function() {
-  it('finds profiles', function() {
+  var searchBox = element(by.model('searchCtrl.searchTerm'));
+  var searchButton = element(by.className('btn'));
+
+  beforeEach(function() {
     browser.get('http://localhost:8080');
+  })
+  it ('has a title', function() {
+    expect(browser.getTitle()).toEqual('Github user search');
+  });
 
-    element(by.model('searchCtrl.searchTerm')).sendKeys('spike01');
-    element(by.className('btn')).click();
-
-    expect(element(by.binding('user.login')).getText()).toEqual('spike01');
+  it('finds profiles', function() {
+    searchBox.sendKeys('spike0');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    expect(profiles.get(0).getText()).toEqual('spike01');
 
   })
 });
