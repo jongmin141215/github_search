08e6dc231de5f775cc5e90439029585797437345
Jongmin Kim <kjm141215@gmail.com>
2016-02-12 19:14:22 -0800

    Display the number of profiles found

--- test/e2e/githubProfileFeature.js
+++ test/e2e/githubProfileFeature.js
@@ -14,6 +14,13 @@ describe('Github Profile finder', function() {
     searchButton.click();
     var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
     expect(profiles.get(0).getText()).toEqual('spike01');
-
   })
+
+  it('displays the number of profiles found', function() {
+    searchBox.sendKeys('jongmin');
+    searchButton.click();
+    var profiles = element.all(by.repeater('user in searchCtrl.searchResult.items'));
+    var num = 30 // change hard-coded number to variable
+    expect(element(by.className('results')).getText()).toEqual(num + ' profiles found')
+  });
 });
